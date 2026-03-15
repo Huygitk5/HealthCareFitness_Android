565a12d8063ff2b684442a8584baae1a02c3b3a2
minh <minhminh20925@gmail.com>
2026-03-15 15:01:41 +0700

    Update Free Workout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     // Thư viện vẽ biểu đồ
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    // Thêm Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
