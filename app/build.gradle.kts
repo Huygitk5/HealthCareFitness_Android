2a8bd139303f1bdb01ef3ad90e15d0e924393b97
Huy <quochuydoan2005@gmail.com>
2026-03-16 09:36:54 +0700

    fix Nav bot workout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,4 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     // Thêm thư viện tải ảnh Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
 }
