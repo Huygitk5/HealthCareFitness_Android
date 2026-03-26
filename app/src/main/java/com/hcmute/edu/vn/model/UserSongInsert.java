762aa6dc30fa04318e7b9d47dad7a1b64ebc3146
Luong Quang Lam <laulua1223@gmail.com>
2026-03-26 15:20:51 +0700

    create relative req model

--- app/src/main/java/com/hcmute/edu/vn/model/UserSongInsert.java
+++ app/src/main/java/com/hcmute/edu/vn/model/UserSongInsert.java
@@ -1,4 +1,24 @@
 package com.hcmute.edu.vn.model;
 
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Request body khi INSERT vào bảng `user_songs` trên Supabase.
+ * Liên kết một user với một bài hát họ đã upload.
+ */
 public class UserSongInsert {
+
+    @SerializedName("user_id")
+    private String userId;
+
+    @SerializedName("song_id")
+    private long songId;
+
+    public UserSongInsert(String userId, long songId) {
+        this.userId = userId;
+        this.songId = songId;
+    }
+
+    public String getUserId() { return userId; }
+    public long getSongId()   { return songId; }
 }
