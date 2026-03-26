38846b4af96e1f1de80fd85e2d6018361b785cf0
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2026-03-26 23:03:24 +0700

    clean up trash

--- app/src/main/java/com/hcmute/edu/vn/activity/CustomExerciseSelectionActivity.java
+++ app/src/main/java/com/hcmute/edu/vn/activity/CustomExerciseSelectionActivity.java
@@ -1,6 +1,7 @@
 package com.hcmute.edu.vn.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
