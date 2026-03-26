08fcc9fce8e26c9d1ccbe382d654da0aaacbd9ad
Ngoc Manh <ngocmanhnctphysic@gmail.com>
2026-03-26 23:01:50 +0700

    clean up trash

--- app/src/main/java/com/hcmute/edu/vn/activity/CustomExerciseSelectionActivity.java
+++ app/src/main/java/com/hcmute/edu/vn/activity/CustomExerciseSelectionActivity.java
@@ -93,7 +93,7 @@ public class CustomExerciseSelectionActivity extends AppCompatActivity {
 
         // Bước 3: HIGHLIGHT
         tvStep3Circle.setBackgroundResource(R.drawable.step_circle_selected);
-        tvStep3Circle.setTextColor(android.graphics.Color.WHITE);
+        tvStep3Circle.setTextColor(Color.WHITE);
         tvStep3Title.setTextColor(android.graphics.Color.parseColor("#589A8D"));
         tvStep3Title.setTypeface(null, android.graphics.Typeface.BOLD);
     }
